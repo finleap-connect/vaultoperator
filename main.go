91d1cd63f59c4e8c69768082c0962069d0b8d9c8
Jan Steffen <jan.steffen@finleap.com>
2022-06-23 15:02:20 +0200

    Add webhook configuration
    Signed-off-by: Jan Steffen <jan.steffen@finleap.com>

--- main.go
+++ main.go
@@ -140,6 +140,10 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "VaultSecret")
 		os.Exit(1)
 	}
+
+	if err = (&vaultv1alpha1.VaultSecret{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "VaultSecret")
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
